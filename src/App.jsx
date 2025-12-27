fba2924f529d41d6e5928be1f402b881643f1b69
Saksham091 <krishnaagrwal4@gmail.com>
2025-12-28 02:18:14 +0530

    Contact Section Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Features />
       <Story />
+      <Contact />
     </main>
   )
 }
